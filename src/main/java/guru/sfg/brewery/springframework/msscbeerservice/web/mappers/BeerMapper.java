643e5fbcb8cb00aa09c16ac24a0cff4284ee0f28
Eder <eder.sdz@gmail.com>
2024-05-15 22:38:04 -0300

    Closes #1 - Refactor Model do Common Package

---
+++ src/main/java/guru/sfg/brewery/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,20 @@
+package guru.sfg.brewery.springframework.msscbeerservice.web.mappers;
+
+import guru.sfg.brewery.springframework.msscbeerservice.domain.Beer;
+import guru.sfg.brewery.model.BeerDto;
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-25.
+ */
+@Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerMapperDecorator.class)
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    BeerDto beerToBeerDtoWithInventory(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}
---
+++ src/test/java/guru/sfg/brewery/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.sfg.brewery.springframework.msscbeerservice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MsscBeerServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
